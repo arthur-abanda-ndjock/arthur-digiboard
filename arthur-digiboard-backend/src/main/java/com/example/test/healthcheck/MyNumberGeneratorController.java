bb4c60392daac515e18faaefcb317f7027825fea
arthur-abanda-ndjock <ndjockabanda_a@yahoo.fr>
2023-11-13 13:43:13 +0100

    test caching 2

--- arthur-digiboard-backend/src/main/java/com/example/test/healthcheck/MyNumberGeneratorController.java
+++ arthur-digiboard-backend/src/main/java/com/example/test/healthcheck/MyNumberGeneratorController.java
@@ -26,7 +26,7 @@ public class MyNumberGeneratorController {
 
 		double expenses = getRandomDouble(60000, 100000);
 
-		System.out.println("hi");
+		System.out.println("hi hihih");
 
 		CrmDashboardCard response = new CrmDashboardCard(clients, admin, expenses);
 		ResponseEntity<CrmDashboardCard> a = new ResponseEntity<CrmDashboardCard>(response,
